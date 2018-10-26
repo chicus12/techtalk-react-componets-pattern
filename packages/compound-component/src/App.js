4080b4ee422436bd7fd9854af5efa71ba64919b6
Francisco Cerdas <chicus12@gmail.com>
2018-10-26 16:58:11 -0600

    Advanced compound component

--- packages/compound-component/src/App.js
+++ packages/compound-component/src/App.js
@@ -4,45 +4,91 @@ import classNames from 'classnames'
 import './App.css'
 
 class Tabs extends Component {
+  static TabsList = ({ children, onSelect, activeIndex }) =>
+    React.Children.map(children, (child, index) =>
+      React.cloneElement(child, {
+        isActive: activeIndex === index,
+        onSelect: () => onSelect(index),
+      })
+    )
+
+  static Tab = ({ children, isActive, onSelect, disabled }) => {
+    const classes = classNames('tab', { active: isActive, disabled })
+    return (
+      <div className={classes} onClick={disabled ? null : onSelect}>
+        {children}
+      </div>
+    )
+  }
+
+  static Panels = ({ children, activeIndex }) => (
+    <div className="panels">
+      {React.Children.toArray(children)[activeIndex]}
+    </div>
+  )
+
+  static Panel = ({ children, ...props }) => {
+    console.log('genious')
+    return <div {...props}>{children}</div>
+  }
+
+  static defaultProps = {
+    tabsOnBottom: false,
+    disabled: [],
+  }
+
   state = { activeIndex: 0 }
 
-  selectTabIndex(activeIndex) {
+  selectTabIndex = activeIndex => {
     this.setState({ activeIndex })
   }
 
-  renderTabs() {
-    return this.props.data.map((tab, index) => {
-      const isActive = this.state.activeIndex === index
-      const classes = classNames('tab', { active: isActive })
-
-      return (
-        <div
-          key={tab.label}
-          className={classes}
-          onClick={() => this.selectTabIndex(index)}
-        >
-          {tab.label}
-        </div>
-      )
-    })
-  }
-
-  renderPanel() {
-    const tab = this.props.data[this.state.activeIndex]
-    return <div>{tab.content}</div>
-  }
-
   render() {
-    return (
-      <div>
-        <div>{this.renderTabs()}</div>
-        <div className="panels">{this.renderPanel()}</div>
-      </div>
+    const { children } = this.props
+    const { activeIndex } = this.state
+
+    return React.Children.map(children, child =>
+      React.cloneElement(child, {
+        onSelect: this.selectTabIndex,
+        activeIndex,
+      })
     )
   }
 }
 
+const TabsCompose = ({ tabs }) => (
+  <div>
+    <Tabs>
+      <Tabs.TabsList>
+        {tabs.map(tab => (
+          <Tabs.Tab key={tab.label}>{tab.label}</Tabs.Tab>
+        ))}
+      </Tabs.TabsList>
+      <Tabs.Panels>
+        {tabs.map(tab => (
+          <Tabs.Panel key={tab.label}>{tab.content}</Tabs.Panel>
+        ))}
+      </Tabs.Panels>
+    </Tabs>
+  </div>
+)
+
 const App = () => {
+  const tabData2 = [
+    {
+      label: 'Tacos2',
+      content: <p>Tacos son deliciosos</p>,
+    },
+    {
+      label: 'Burritos2',
+      content: <p>Algunas veces un burrito es lo que realmente necesitas</p>,
+    },
+    {
+      label: 'Coconut Korma2',
+      content: <p>Puede ser la mejor opción</p>,
+    },
+  ]
+
   const tabData = [
     {
       label: 'Tacos',
@@ -60,7 +106,8 @@ const App = () => {
 
   return (
     <div>
-      <Tabs data={tabData} />
+      <TabsCompose tabs={tabData} />
+      <TabsCompose tabs={tabData2} />
     </div>
   )
 }
